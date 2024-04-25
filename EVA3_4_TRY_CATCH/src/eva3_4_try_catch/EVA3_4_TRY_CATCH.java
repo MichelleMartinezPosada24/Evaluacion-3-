b23f2ca621a6bafd637d40187aaa16e94c9f4d81
MichelleMartinezPosada24 <158379972+MichelleMartinezPosada24@users.noreply.github.com>
2024-04-24 18:44:33 -0600

    Add files via upload

---
+++ EVA3_4_TRY_CATCH/src/eva3_4_try_catch/EVA3_4_TRY_CATCH.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_4_try_catch;
+
+/**
+ *
+ * @author miche
+ */
+public class EVA3_4_TRY_CATCH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // aritmeticexceception 
+       int x =10, y = 0;
+       int resu = 0;
+       try{ 
+           resu = x / y;
+       }
+       
+       }cath (AritmeticException e){//aqui va el 
+       }   
+        System.out.println("Resultado = " + resu);
+        
+    }
+    
+
